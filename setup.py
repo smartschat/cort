13f56cf7bb7538936787fa3d0ecd0aabe7aa2e52
smartschat <sebastian.martschat@gmail.com>
2015-06-01 03:12:14 +0200

    Updated to status of NAACL'15 paper.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='cort',
-    version='0.1.1',
+    version='0.1.4',
     packages=['cort',
               'cort.analysis',
               'cort.core',
