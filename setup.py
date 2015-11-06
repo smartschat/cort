de97804bb3483726df21fd34104c9f4aff155435
smartschat <sebastian.martschat@gmail.com>
2015-11-06 15:57:01 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='cort',
-    version='0.2.4.2',
+    version='0.2.4.3',
     packages=['cort',
               'cort.analysis',
               'cort.core',
